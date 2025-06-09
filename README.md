219efdf885c803f81e0b621ed99037e09fb3abbc
Dreamwalker <aristojeff@gmail.com>
2025-06-09 19:30:43 +0900

    add starter files

---
+++ README.md
@@ -0,0 +1,3 @@
+# ai_sprint_25_gemini_live
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/dreamwalker/ai_sprint_25_gemini_live/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.dreamwalker.ai_sprint_25_gemini_live
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
