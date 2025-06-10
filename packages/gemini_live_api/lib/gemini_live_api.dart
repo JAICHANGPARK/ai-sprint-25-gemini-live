236310b03975ec2262e43db01048658c1deae3c8
Dreamwalker <aristojeff@gmail.com>
2025-06-10 20:20:17 +0900

    add custom gemini live pacakge

---
+++ packages/gemini_live_api/lib/gemini_live_api.dart
@@ -0,0 +1,1 @@
+export 'src/google_genai.dart';
---
+++ packages/gemini_live_api/test/gemini_live_api_test.dart
@@ -0,0 +1,9 @@
+import 'package:flutter_test/flutter_test.dart';
+
+import 'package:gemini_live_api/gemini_live_api.dart';
+
+void main() {
+  test('adds one to input values', () {
+
+  });
+}
